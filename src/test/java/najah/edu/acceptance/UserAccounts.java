089173b64cebcfe193cd45a8e9059a7869e37f32
laithalayyan <laith.nnn96@gmail.com>
2023-12-23 03:30:34 +0200

    sonar pre-final stage

--- src/test/java/najah/edu/acceptance/UserAccounts.java
+++ src/test/java/najah/edu/acceptance/UserAccounts.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertTrue;
 import static roles.User.*;
 
 public class UserAccounts {
-
     @Given("they choose list users")
     public void they_choose_list_users() {
         setListAcc(true);
@@ -15,5 +14,9 @@ public class UserAccounts {
     @Then("the users list will appear")
     public void the_users_list_will_appear() {
         listUserAccounts();
+    }
+
+    @And("they choose list userss")
+    public void theyChooseListUserss() {
     }
 }
--- use_cases/Users.feature
+++ use_cases/Users.feature
@@ -2,4 +2,5 @@ Feature: Admin Manage UsersAccounts
 
   Scenario: Admin choose to list the accounts
     Given they choose list users
+    And they choose list userss
     Then the users list will appear
